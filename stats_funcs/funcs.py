77154fbfd54e4edd3071d43c9141b639bd5f4498
Lucas Corbanez <lucascorbanez@gmail.com>
2022-05-28 20:13:22 -0300

    Sep Interp and Sampler

--- stats_funcs/funcs.py
+++ stats_funcs/funcs.py
@@ -3,26 +3,7 @@ from numpy import array
 from scipy.special import erf
 from scipy.integrate import quad
 from random import uniform
-from math import ceil, factorial
-
-def uniform_distribution(n: int, x_min: float, x_max: float) -> array:
-    '''
-    Generates an array with uniformly distributed numbers, i.e., a uniform distribution
-
-        Parameters:
-            n (int): number of values in a sample;
-            x_min (float): minimum value of the sample;
-            x_max (float): maximum value of the sample.
-
-        Returns:
-            temp (array): array with uniform distribution.
-    '''
-    temp = array([])
-
-    for i in range(n):
-        temp = np.append(temp, uniform(x_min, x_max))
-
-    return temp
+from math import ceil
 
 def sampler(x: array, pdf_x: array) -> array:
     '''
@@ -35,15 +16,18 @@ def sampler(x: array, pdf_x: array) -> array:
         Returns:
             temp (array): sample of N values distributed accordingly to P({x}).
     '''
+    x = x/max(x)
     temp = array([])
     for i in range(len(pdf_x)):
-        temp = np.append(temp, uniform_distribution(ceil(len(x)*pdf_x[i]), 0, x[i]))
+        temp = np.append(temp, uniform(ceil(100*pdf_x[i]), x[i] - 0.05, x[i] + 0.05))
+    temp = np.random.choice(temp, len(x), replace=False)
 
-    if len(temp) < len(x):
-        temp = np.append(temp, uniform_distribution(len(x)-len(temp), min(x), max(x)))
-    else:
-        temp = np.random.choice(temp, len(x), replace=False)
-    return temp
+    # if len(temp) < len(x):
+    #     temp = np.append(temp, uniform(len(x) - len(temp), min(x), max(x)))
+    # else:
+    #     temp = np.random.choice(temp, len(x), replace=False)
+
+    return temp/max(temp)
 
 def gaussian(x: float, mu: float, sigma: float) -> float:
     '''
@@ -134,93 +118,26 @@ def skewed_distribution(x: array, x_0: float, sigma: float) -> array:
 
     return sampler(x, temp)
 
-def binomial_coef(n: int, r: int) -> float:
-    return(factorial(n))/(factorial(r))*(factorial(n-r))
-
-def binomial_pdf(r: int, p: float, n: int) -> float:
-    '''
-    Calculates the value of the probability density function of a binomial distribution
-    
-        Parameters: 
-            r (int): sample;
-            p (float): probability of a certain event.
-            
-        Returns:
-            Distribution (array): Binomial Distribution.   
-    '''
-    if p > 1:
-        raise ValueError("p must be a value smaller than 1.")
-    else:
-        return (p**r)*((1-p)**(n-r))*binomial_coef(n, r)
-
-def binomial_distribution(x: array, p: float) -> array:
-    '''
-    Creates a binomial distribution
-    
-        Parameters: 
-            x (array): sample;
-            p (float): probability of a certain event.
-            
-        Returns:
-            Distribution (array): Binomial Distribution.   
-    '''
-    n = len(x)
-    temp = array([])
-    for r in x:
-        temp = np.append(temp, binomial_pdf(r, p, n))
-
-    return sampler(x, temp)
-
-def linear_interpolation(f1: float, f2: float, x1: float, x2: float, _x: float) -> float:
-    '''
-    Calculates the linear_interpolation expression
-    
-        Parameters:
-            f1 and f2 (float): functions indexed values;
-            x1 and x2 (float): sample indexed values;
-            _x (float): mean of sample values.
-            
-        Returns:
-            \bar{P}_{1,2} (float): linearly interpolated value of f1 and f2.
-    '''
-    return (f1 + (f2 - f1)*(_x - x1)/(x2 - x1))
-
-def fill_sample(x: array) -> array:
-    '''
-    Creates an array filled with mean values in between of every two values
-
-        Parametes:
-            x (array): sample with N values.
-
-        Returns:
-            _x (array): filled sample with, I'm guessing, 2N-1 values.
-    '''
-    temp = array([])
-    for i in range(len(x) - 1):
-        temp = np.append(temp, x[i]) 
-        temp = np.append(temp, np.mean([x[i],x[i+1]]))
-    temp = np.append(temp, x[-1])
-    
-    return temp
-
 def interpolate(f: array, x: array) -> array:
     '''
-    Interpolates a distribution and fills a sample value array
-
+    Linear interpolation of a given function
+        
         Parameters:
-            f (array): distribution calculated from x sample;
-            x (array): sample with N values.
+            f (array): array containing N values f(x);
+            x (array): array containing the domain of f with N values.
 
-        Returns:
-            tup (tuple): tuple with interpolated distribution and filled array with matching dimensions.
-    '''
-    temp = array([])
-    temp_x = fill_sample(x)
-    for i in range(0, len(temp_x)-1, 2):
-        temp = np.append(temp, f[int(i/2)]) 
-        temp = np.append(temp, linear_interpolation(f[int(i/2)], f[int(i/2)+1], temp_x[i], temp_x[i+2], temp_x[i+1]))
+        Return
+            _f (array): array containing 2N - 1 interpolated values of f(x). 
+    ''' 
+    temp = ([])
+    if len(f) != len(x):
+        raise ValueError("Dim(f) must be equal to Dim(x).")
+    elif len(f) == len(x):
+        for i in range(len(x)-1):
+            a_i = (f[i+1] - f[i])/(x[i+1] - x[i])
+            b_i = f[i] - a_i*x[i]
+            temp = np.append(temp, f[i])
+            temp = np.append(temp, a_i*uniform(x[i], x[i+1]) + b_i)
     temp = np.append(temp, f[-1])
     
-    tup = (temp, temp_x)
-
-    return tup
+    return temp
