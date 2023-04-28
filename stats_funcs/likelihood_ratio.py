966df2ff21624a45d20f1211c00d72c39636c5c3
Lucas Corbanez <lucascorbanez@gmail.com>
2023-04-28 07:25:11 -0300

    Minimization parallel computing

--- stats_funcs/likelihood_ratio.py
+++ stats_funcs/likelihood_ratio.py
@@ -14,43 +14,89 @@ import logging
 import sys
 
 """
-params = [Omega Lambda, Omega Matter, Hubble's Constant, Omega Curvature, Omega Radiation, w]
+params = [Omega Lambda, Omega Matter, Hubble's Constant, w]
 """
 
-def Dc(z, Omega_l, Omega_m, Omega_k, w):
-    def f(z, Omega_l, Omega_m, Omega_k, w):
+def cross_product(*linspaces):
+    # Create a list to hold the linspaces and their lengths
+    linspaces_list = []
+    lengths = []
+
+    # Loop through each linspace and get its values and length
+    for linspace in linspaces:
+        linspaces_list.append(linspace)
+        lengths.append(len(linspace))
+
+    # Create a 2D array to hold the cross product
+    cross_product_array = np.zeros((np.prod(lengths), len(linspaces)))
+
+    # Loop through each value in the linspaces and add it to the cross product array
+    for i in range(len(linspaces)):
+        repeat = np.prod(lengths[i+1:])
+        cross_product_array[:, i] = np.tile(np.repeat(linspaces[i], repeat), np.prod(lengths[:i]))
+
+    return cross_product_array
+
+def Dc(z, Omega_l, Omega_m, w):
+    Omega_k = 1 - (Omega_l + Omega_m + 9.2e-5)
+    def f(z, Omega_l, Omega_m, w):
         return (np.sqrt(Omega_l*(1 + z)**(-3 - 3*w) + Omega_m*(1 + z)**3 + Omega_k*(1 + z)**2 + 9.2e-5*(1 + z)**4))**(-1)
-    return quad(f, 0, z, args = (Omega_l, Omega_m, Omega_k, w))[0]
+    return quad(f, 0, z, args = (Omega_l, Omega_m, w))[0]
 
-def Dt(z, Omega_l, Omega_m, Omega_k, w):
+def Dt(z, Omega_l, Omega_m, w):
+    Omega_k = 1 - (Omega_l + Omega_m + 9.2e-5)
     if Omega_k > 0:
-        return np.sin(np.sqrt(Omega_k)*Dc(z, Omega_l, Omega_m, Omega_k, w))/np.sqrt(Omega_k)
+        return np.sin(np.sqrt(Omega_k)*Dc(z, Omega_l, Omega_m, w))/np.sqrt(Omega_k)
     elif Omega_k == 0:
-        return Dc(z, Omega_l, Omega_m, Omega_k, w)
+        return Dc(z, Omega_l, Omega_m, w)
     elif Omega_k:
-        return np.sinh(np.sqrt(abs(Omega_k))*Dc(z, Omega_l, Omega_m, Omega_k, w))/np.sqrt(abs(Omega_k))
+        return np.sinh(np.sqrt(abs(Omega_k))*Dc(z, Omega_l, Omega_m, w))/np.sqrt(abs(Omega_k))
 
-def Dl(z, Omega_l, Omega_m, Omega_k, w):
-    return (1 + z) * Dt(z, Omega_l, Omega_m, Omega_k, w)
+def Dl(z, Omega_l, Omega_m, w):
+    return (1 + z) * Dt(z, Omega_l, Omega_m, w)
                
-def mu(z, Omega_l, Omega_m, Omega_k, w, Hubble):
+def mu(z, Omega_l, Omega_m, w, Hubble):
     c = 3e5 #(km/s)
-    return 5*np.log10(Dl(z, Omega_l, Omega_m, Omega_k, w)) + 25 + 5*np.log10(c/ Hubble)
+    return 5*np.log10(Dl(z, Omega_l, Omega_m, w)) + 25 + 5*np.log10(c/ Hubble)
 
-def chi2_i(mu_o, sigma_o, z_obs, Omega_l, Omega_m, Omega_k, w, Hubble):
-    return ((mu(z_obs, Omega_l, Omega_m, Omega_k, w, Hubble) - mu_o)**2)/sigma_o**2
+def chi2_i(mu_o, sigma_o, z_obs, Omega_l, Omega_m, w, Hubble):
+    return ((mu(z_obs, Omega_l, Omega_m, w, Hubble) - mu_o)**2)/sigma_o**2
 
-def chi2(Omega_l, Omega_m, Omega_k, w, Hubble, df): 
+def chi2(Omega_l, Omega_m, w, Hubble, df): 
     values = []
     for idx in df.index:
-        values.append(chi2_i(df.mu[idx], df.sigma[idx], df.z[idx], Omega_l, Omega_m, Omega_k, w, Hubble))
+        values.append(chi2_i(df.mu[idx], df.sigma[idx], df.z[idx], Omega_l, Omega_m, w, Hubble))
     return sum(values)
 
-def ratio(Omega_l, Omega_m, Omega_k, w, Hubble, df, chi2null):
-    return chi2(Omega_l, Omega_m, Omega_k, w, Hubble, df) - chi2null
+def ratio(Omega_l, Omega_m, w, Hubble, df, chi2null):
+    return chi2(Omega_l, Omega_m, w, Hubble, df) - chi2null
+
+def _minimize(args):
+
+    dict_kwargs = {
+        'fun': h,
+        'x0': args[2],
+        'args': (args[0], args[1]),
+        'method': 'Nelder-Mead',
+        'jac': None,
+        'hessp': None,
+        'hess': None,
+        'constraints': (),
+        'tol': 1e-3,
+        'callback': None,
+        'options': None,
+    }
+    _min = minimize(**dict_kwargs)
+
+    _vars = _zip_vars(args[0], _min.x)
+
+    keys = ['Omega_l', 'Omega_m', 'w', 'Hubble']
+    values = [args[0].get(key,  _vars.get(key)) for key in keys]
+
+    return values
 
 def _zip_vars(fixed: dict, x) -> dict:
-    full_parameters = ['Omega_l', 'Omega_m', 'Omega_k', 'w',  'Hubble']
+    full_parameters = ['Omega_l', 'Omega_m', 'w',  'Hubble']
     variable_parameters = []
     for param in full_parameters:
         if param not in list(fixed.keys()):
@@ -77,25 +123,32 @@ def h(x, fixed, df):
     return g(**_vars) 
 
 def generate_grid(config, grid_size):
-    fixed = {}
-    x0 = config['minimization_seed']
-    for i in np.linspace(config['grid'][config['free'][0]][0], config['grid'][config['free'][0]][1], grid_size):
-        fixed[config['free'][0]] = i
-        for j in np.linspace(config['grid'][config['free'][1]][0], config['grid'][config['free'][1]][1], grid_size):
-            fixed[config['free'][1]] = j
-            
-            minimum = minimize(h, x0 = x0, args = (fixed, config['df']), method = 'Nelder-Mead', tol = 1e-6)
-            x0 = minimum.x
-            
-            _vars = _zip_vars(fixed, minimum.x)
-            
-            keys = ['Omega_l', 'Omega_m', 'Omega_k', 'w', 'Hubble']
-            values = [fixed.get(key,  _vars.get(key)) for key in keys]
-            print(f'Set of parameters to calculate ratio: {values}')
-            yield tuple(values)
+    
+    def generate_free_params(config, grid_size):
+        standard_x0 = {'Omega_l':0.74, 'Omega_m':0.26, 'w':-1,  'Hubble':70}
+        _linspaces = []
+        for free in config['free']:
+            _linspaces.append(np.linspace(config['grid'][free][0], config['grid'][free][1], grid_size))
+        cross = cross_product(*_linspaces)
+        for i in cross:
+
+            fixed = {config['free'][j]: i[j] for j in range(len(config['free']))}
+            dict1 = {key: value for key, value in standard_x0.items() if key not in fixed}
+            standard_x0_values = [value for value in dict1.values()]
+
+            yield fixed, config['df'], standard_x0_values
+    
+    
+    with Pool() as pool:
+        results = pool.map(_minimize, generate_free_params(config, grid_size))
+        pool.close()
+        pool.join()
+
+    for result in results:
+        yield  result
 
 def orchestrator(params, **kwargs):
-    return params, ratio(params[0], params[1], params[2], params[3], params[4], kwargs['df'], kwargs['chi2null'])
+    return params, ratio(params[0], params[1], params[2], params[3], kwargs['df'], kwargs['chi2null'])
     
 if __name__ == '__main__':
 
@@ -112,14 +165,14 @@ if __name__ == '__main__':
     config['df'] = df
     fixed = {}
 
-    x0 = [0.76, 0.24, 0.02, -1, 71] #['Omega_l', 'Omega_m', 'Omega_k', 'w',  'Hubble']
+    x0 = [0.76, 0.24, -1, 71] #['Omega_l', 'Omega_m', 'w',  'Hubble']
 
-    null = minimize(h, x0 = x0, args = (fixed, df), method = 'Nelder-Mead', tol = 1e-6, options = {'maxiter': 10000})
+    null = minimize(h, x0 = x0, args = (fixed, df), method = 'Nelder-Mead', tol = 1e-6, bounds = ((0,1), (0,1), (-1.5, 0), (0, None)), options = {'maxiter': 10000})
     config['minimum'] = null.x
     logging.info(f'Maximum Likelihood Estimators: {null.x}')
 
 
-    chi2null = chi2(null.x[0], null.x[1], null.x[2], null.x[3], null.x[4], df)
+    chi2null = chi2(null.x[0], null.x[1], null.x[2], null.x[3], df)
     dict_kwargs = {'df': df, 'chi2null': chi2null}
 
     for grid_size in config['grid_sizes']:
@@ -146,9 +199,8 @@ if __name__ == '__main__':
         # Brace yourself for some terrible coding!!! ლ(ಠ益ಠლ)
         out = {'Omega_l': np.array(list(np.array(results)[:,0]))[:,0].tolist(),
                'Omega_m': np.array(list(np.array(results)[:,0]))[:,1].tolist(),
-               'Omega_k': np.array(list(np.array(results)[:,0]))[:,2].tolist(),
-               'w': np.array(list(np.array(results)[:,0]))[:,3].tolist(),
-               'Hubble': np.array(list(np.array(results)[:,0]))[:,4].tolist(),
+               'w': np.array(list(np.array(results)[:,0]))[:,2].tolist(),
+               'Hubble': np.array(list(np.array(results)[:,0]))[:,3].tolist(),
                'Ratio': np.array(results)[:,1].tolist()}
 
         with open('data/' + file_name, 'w') as out_file:
